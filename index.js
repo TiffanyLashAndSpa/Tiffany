6fa125952222bdf46554d81aa2abdfb444cc7b0c
kylehuang041 <kylehuang041@gmail.com>
2023-01-22 18:02:18 -0800

    fixed mobile menu

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-const menuBtn = document.querySelector('#menuIcon');
-const menu = document.querySelector('.side');
+/**
+ * @file index.js
+ * @author Kyle Huang
+ * 
+ * Contains the menu toggle and updates year in footer.
+ */
 
-const toggleMenu = () => {
-    menu.classList.toggle('show');
-    console.log("Testing");
-}
+"use strict";
+(function () {
+	window.addEventListener("load", init);
 
-menuBtn.addEventListener('click', toggleMenu);
+	/**
+	 * @brief main block
+	 */
+	function init() {
+		const menuBtn = document.querySelector('#menuIcon');
+		const menu = document.querySelector('.side');
+
+		// toggle menu in mobile mode
+		menuBtn.addEventListener('click', () => menu.classList.toggle('show'));
+
+		// update year in footer
+		updateFooter();
+	}
+
+	/**
+	 * @brief update year in footer
+	 */
+	function updateFooter() {
+		let yearElem = document.querySelector("#year");
+		let year = new Date().getFullYear();
+		yearElem.textContent = year;
+	}
+})();
